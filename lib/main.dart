91b540d15242583d2a67509bcbffe8459b2cda93
pathustler <patlow.harry@gmail.com>
2023-07-07 08:34:09 +0530

    Delete feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
-import 'dart:developer';
+import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
@@ -13,16 +13,37 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple,
+        secondaryHeaderColor: Colors.purple[900],
+      ),
+      home: const MyHomePage(title: 'MBTI Database'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   List<List<dynamic>> itemList = [];
   List<List<dynamic>> filteredItemList = [];
+
+  final TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -31,38 +52,28 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future<void> loadDataFromStorage() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String>? encodedData = prefs.getStringList('data');
+    final box = GetStorage();
+    String? encodedData = box.read('data');
     if (encodedData != null) {
-      List<List<dynamic>> data = encodedData.map((jsonString) {
-        return List<dynamic>.from(jsonDecode(jsonString));
-      }).toList();
+      List<dynamic> data = jsonDecode(encodedData);
       setState(() {
-        itemList = data;
+        itemList = List<List<dynamic>>.from(data);
         filteredItemList = itemList;
-      });
-    } else {
-      setState(() {
-        itemList = [];
-        filteredItemList = [];
       });
     }
   }
 
-  void updateDataList(List<List<dynamic>> newDataList) {
-    setState(() {
-      itemList.addAll(newDataList);
-      filteredItemList = itemList;
-      saveDataToStorage(itemList);
-    });
+  void updateDataList(List<List<dynamic>> newDataList) async {
+    itemList.addAll(newDataList);
+    filteredItemList = itemList;
+    await saveDataToStorage(itemList);
+    setState(() {});
   }
 
   Future<void> saveDataToStorage(List<List<dynamic>> data) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> encodedData = data.map((list) {
-      return jsonEncode(list);
-    }).toList();
-    await prefs.setStringList('data', encodedData);
+    final box = GetStorage();
+    String encodedData = jsonEncode(data);
+    await box.write('data', encodedData);
   }
 
   void filterItems(String searchTerm) {
@@ -86,45 +97,37 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.deepPurple,
-        secondaryHeaderColor: Colors.purple[900],
-      ),
-      home: MyHomePage(
-        title: 'MBTI Database',
-        itemList: filteredItemList,
-        updateDataList: updateDataList,
-        filterItems: filterItems,
-      ),
-    );
+  void navigateToEditPage(List<dynamic> item) async {
+  List<dynamic>? editedData = await Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => EditPage(item: item, deleteData: deleteData),
+    ),
+  );
+  if (editedData != null) {
+    updateDataList([editedData]); // Wrap editedData in a list
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-    required this.title,
-    required this.itemList,
-    required this.updateDataList,
-    required this.filterItems,
-  }) : super(key: key);
+  void navigateToAddPage() async {
+    List<List<dynamic>> newDataList = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AddPage(),
+      ),
+    );
+    if (newDataList != null) {
+      updateDataList(newDataList);
+    }
+  }
 
-  final String title;
-  final List<List<dynamic>> itemList;
-  final Function(List<List<dynamic>>) updateDataList;
-  final Function(String) filterItems;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void deleteData(List<dynamic> item) async {
+  itemList.remove(item);
+  filteredItemList = itemList;
+  await saveDataToStorage(itemList);
+  setState(() {});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -147,7 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.all(8.0),
               child: TextField(
                 controller: searchController,
-                onChanged: widget.filterItems,
+                onChanged: filterItems,
                 decoration: const InputDecoration(
                   labelText: 'Search',
                   prefixIcon: Icon(Icons.search),
@@ -156,11 +159,15 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: widget.itemList.length,
+                itemCount: filteredItemList.length,
                 itemBuilder: (context, index) {
+                  String mbtiType = filteredItemList[index][1];
+                  Color containerColor = _getContainerColor(mbtiType);
+
                   return ListTile(
+                    onTap: () => navigateToEditPage(filteredItemList[index]),
                     title: Text(
-                      widget.itemList[index][0],
+                      filteredItemList[index][0],
                       style: const TextStyle(
                         fontFamily: 'Roboto',
                         fontSize: 16.0,
@@ -171,15 +178,15 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 80.0,
                       padding: const EdgeInsets.all(13.0),
                       decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 63, 170, 10),
+                        color: containerColor,
                         borderRadius: BorderRadius.circular(10.0),
                       ),
                       child: Text(
-                        widget.itemList[index][1],
+                        filteredItemList[index][1],
                         textAlign: TextAlign.center,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          color: Color.fromARGB(150, 0, 0, 0),
                         ),
                       ),
                     ),
@@ -191,20 +198,40 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          List<List<dynamic>> newDataList = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddPage(),
-            ),
-          );
-          if (newDataList != null) {
-            widget.updateDataList(newDataList);
-          }
-        },
-        child: const Icon(Icons.add),
+        backgroundColor: const Color.fromARGB(255, 200, 60, 185),
+        onPressed: navigateToAddPage,
+        child: const CircleAvatar(
+          backgroundColor: Color.fromARGB(255, 200, 60, 185),
+          foregroundColor: Colors.black,
+          child: Icon(Icons.add),
+        ),
       ),
     );
+  }
+
+  Color _getContainerColor(String mbtiType) {
+    if (mbtiType == 'ISTJ' ||
+        mbtiType == 'ESTJ' ||
+        mbtiType == 'ESFJ' ||
+        mbtiType == 'ISFJ') {
+      return const Color.fromARGB(255, 93, 194, 219);
+    } else if (mbtiType == 'INTJ' ||
+        mbtiType == 'ENTJ' ||
+        mbtiType == 'ENTP' ||
+        mbtiType == 'INTP') {
+      return const Color.fromARGB(255, 141, 101, 125);
+    } else if (mbtiType == 'INFP' ||
+        mbtiType == 'ENFP' ||
+        mbtiType == 'INFJ' ||
+        mbtiType == 'ENFJ') {
+      return const Color.fromARGB(255, 104, 174, 139);
+    } else if (mbtiType == 'ISTP' ||
+        mbtiType == 'ESTP' ||
+        mbtiType == 'ESFP' ||
+        mbtiType == 'ISFP') {
+      return const Color.fromARGB(255, 245, 215, 92);
+    }
+    return Colors.transparent; // Default color if no matching MBTI type
   }
 }
 
@@ -238,20 +265,11 @@ class _AddPageState extends State<AddPage> {
     'INTJ',
   ];
 
-  void addData() async {
+  void addData() {
     String name = nameController.text;
     List<dynamic> newData = [name, selectedMBTI];
     List<List<dynamic>> newDataList = [newData];
-    await saveDataToStorage(newDataList);
     Navigator.pop(context, newDataList);
-  }
-
-  Future<void> saveDataToStorage(List<List<dynamic>> data) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> encodedData = data.map((list) {
-      return jsonEncode(list);
-    }).toList();
-    await prefs.setStringList('data', encodedData);
   }
 
   @override
@@ -338,7 +356,8 @@ class _AddPageState extends State<AddPage> {
             ElevatedButton(
               onPressed: addData,
               style: ElevatedButton.styleFrom(
-                primary: Colors.deepPurple,
+                primary: const Color.fromARGB(255, 200, 60, 185),
+                foregroundColor: Colors.black,
                 textStyle: const TextStyle(
                   fontFamily: 'Roboto',
                   fontSize: 16.0,
@@ -353,3 +372,203 @@ class _AddPageState extends State<AddPage> {
     );
   }
 }
+
+class EditPage extends StatefulWidget {
+  final List<dynamic> item;
+  final Function(List<dynamic>) deleteData;
+
+  EditPage({required this.item, required this.deleteData});
+
+  @override
+  _EditPageState createState() => _EditPageState();
+}
+
+
+class _EditPageState extends State<EditPage> {
+  String selectedMBTI = '';
+  final TextEditingController nameController = TextEditingController();
+
+  List<String> mbtiTypes = [
+    'ESFP',
+    'ESFJ',
+    'ESTP',
+    'ESTJ',
+    'ENFP',
+    'ENFJ',
+    'ENTP',
+    'ENTJ',
+    'ISFP',
+    'ISFJ',
+    'ISTP',
+    'ISTJ',
+    'INFP',
+    'INFJ',
+    'INTP',
+    'INTJ',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = widget.item[0];
+    selectedMBTI = widget.item[1];
+  }
+
+  void editData() {
+    String name = nameController.text;
+    List<dynamic> editedData = [name, selectedMBTI];
+    Navigator.pop(context, editedData);
+  }
+
+  void deleteData() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Confirmation"),
+        content: const Text("Are you sure you want to delete this item?"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, true); // Return true to indicate delete action
+            },
+            child: const Text("Delete"),
+          ),
+        ],
+      ),
+    ).then((result) {
+      if (result == true) {
+        Navigator.pop(context, null); // Return null to indicate delete action
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 21, 30, 40),
+        centerTitle: true,
+        title: const Text(
+          'Edit Item',
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: Container(
+        color: const Color.fromARGB(255, 21, 30, 40),
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              'Name',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 18.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            TextField(
+              controller: nameController,
+              style: const TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[900],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'MBTI Type',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 18.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            DropdownButton<String>(
+              value: selectedMBTI,
+              icon: const Icon(Icons.arrow_drop_down),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),
+              dropdownColor: Colors.grey[900],
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedMBTI = newValue!;
+                });
+              },
+              items: mbtiTypes.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: editData,
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 200, 60, 185),
+                      foregroundColor: Colors.black,
+                      textStyle: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    child: const Text('Submit'),
+                  ),
+                ),
+                const SizedBox(width: 20.0),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      widget.deleteData(widget.item); // Delete the item
+                      Navigator.pop(context);
+                  },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      foregroundColor: Colors.white,
+                      textStyle: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    child: const Text('Delete'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
